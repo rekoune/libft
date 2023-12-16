a2062132ff6ee39b36d3c8af9a9c063fca89b737
Abdellah Rekoune <arekoune@c2r6p2.1337.ma>
2023-12-16 15:33:08 +0100

    done

---
+++ ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arekoune <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 15:20:03 by arekoune          #+#    #+#             */
+/*   Updated: 2023/12/16 15:20:12 by arekoune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*c;
+	int		i;
+	int		s1len;
+	int		s2len;
+
+	i = 0;
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	c = malloc ((s1len + s2len + 1) * sizeof (char));
+	if (c == 0)
+		return (0);
+	ft_strlcpy(c, s1, s1len + 1);
+	ft_strlcat(c, s2, s2len + s1len + 1);
+	return (c);
+}
